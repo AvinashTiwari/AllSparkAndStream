225b7c3df847df9a13a6fbc085220e4a72d510f7
avinash\avinash.t <qwe123kids@gmail.om>
2019-02-28 19:21:55 -0700

    reduce

---
+++ 12_Java_Spark_project/spark-mapping/src/main/java/learn/avinash/spark/Main.java
@@ -0,0 +1,38 @@
+package learn.avinash.spark;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+               List<Integer> inputdata = new ArrayList<Integer>();
+               inputdata.add(35);
+               inputdata.add(135);
+               inputdata.add(235);
+               inputdata.add(335);
+
+        Logger.getLogger("org.apache").setLevel(Level.WARN);
+
+        SparkConf conf = new SparkConf()
+                .setAppName("StartingSpark")
+                .setMaster("local[*]");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaRDD<Integer> myrdd = sc.parallelize(inputdata);
+        Integer result = myrdd.reduce(( value1,  value2) -> value1 + value2);
+        JavaRDD<Double> sqrtRDD = myrdd.map(value -> Math.sqrt(value));
+        sqrtRDD.foreach(value -> System.out.println(value));
+
+        System.out.println(result);
+
+        sc.close();
+
+    }
+}
