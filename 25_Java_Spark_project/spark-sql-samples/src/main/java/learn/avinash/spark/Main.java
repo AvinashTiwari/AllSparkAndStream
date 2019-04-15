e89ff38d3f1533c9a601bab01c2b036d908f60ae
Avinash Tiwari <qwe123kids@gmail.com>
2019-04-14 19:55:24 -0600

    no message

--- 25_Java_Spark_project/spark-sql-samples/src/main/java/learn/avinash/spark/Main.java
+++ 25_Java_Spark_project/spark-sql-samples/src/main/java/learn/avinash/spark/Main.java
@@ -7,41 +7,36 @@ import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
 public class Main {
+    @SuppressWarnings("resource")
+    public static void main(String[] args)
+    {
+        System.setProperty("hadoop.home.dir", "c:/winutils");
+        Logger.getLogger("org.apache").setLevel(Level.WARN);
 
-	@SuppressWarnings("resource")
-	public static void main(String[] args) 
-	{
-		System.setProperty("hadoop.home.dir", "c:/winutils");
-		Logger.getLogger("org.apache").setLevel(Level.WARN);
-		
-	//	SparkConf conf = new SparkConf().setAppName("startingSpark").setMaster("local[*]");
-	//	JavaSparkContext sc = new JavaSparkContext(conf);
+        //	SparkConf conf = new SparkConf().setAppName("startingSpark").setMaster("local[*]");
+        //	JavaSparkContext sc = new JavaSparkContext(conf);
 
-		SparkSession sparkSession = SparkSession.builder().appName("testingSql")
-				.master("local[*]")
-				.config("spark.sql.warehouse.dir","file:///d://tmp/")
-				.getOrCreate();
+        SparkSession sparkSession = SparkSession.builder().appName("testingSql")
+                .master("local[*]")
+                .config("spark.sql.warehouse.dir","file:///d://tmp/")
+                .getOrCreate();
 
         Dataset<Row> dataset = sparkSession.read().option("header", true)
                 .csv("src/main/resources/exams/students.csv");
 
-        dataset.show();
-		long numberOfRow = dataset.count();
-		System.out.println("Number of Rows " + numberOfRow);
 
-        Row firstRow = dataset.first();
-        String subject = firstRow.get(2).toString();
-        System.out.println("Subject " + subject);
 
-        String subjectAs = firstRow.getAs("subject").toString();
-        System.out.println("Subject " + subjectAs);
+        //	Dataset<Row> math = dataset.filter("subject ='Math' And year >= 2007");
+        dataset.createOrReplaceTempView("my_students_table");
+        // Dataset<Row> math = sparkSession.sql("select subject, score, year from my_students_table where subject ='French'");
+        //  Dataset<Row> math = sparkSession.sql("select max( score) from my_students_table where subject ='French'");
+        // Dataset<Row> math = sparkSession.sql("select avg(score) from my_students_table where subject ='French'");
+        Dataset<Row> math = sparkSession.sql("select distinct(year) from my_students_table where subject ='French' order by year");
 
-        int year = Integer.parseInt(firstRow.getAs("year"));
-        System.out.println("year " + year);
-
+        math.show();
         sparkSession.close();
-		
 
-	}
+
+    }
 
 }
