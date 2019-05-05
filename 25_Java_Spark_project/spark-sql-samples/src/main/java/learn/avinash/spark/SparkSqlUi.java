ca6f0080e6e1ab3ac0222485a7ffbd241d161f52
Avinash Tiwari <qwe123kids@gmail.com>
2019-05-05 15:31:06 -0600

    no message

--- 25_Java_Spark_project/spark-sql-samples/src/main/java/learn/avinash/spark/SparkSqlUi.java
+++ 25_Java_Spark_project/spark-sql-samples/src/main/java/learn/avinash/spark/SparkSqlUi.java
@@ -5,12 +5,8 @@ import org.apache.log4j.Logger;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.types.DataTypes;
 
 import java.util.Scanner;
-
-import static org.apache.spark.sql.functions.col;
-import static org.apache.spark.sql.functions.date_format;
 
 public class SparkSqlUi {
     @SuppressWarnings("resource")
@@ -29,18 +25,26 @@ public class SparkSqlUi {
 
 
         Dataset<Row> dataset = sparkSession.read().option("header",true).csv("src/main/resources/biglog.txt");
+        dataset.createOrReplaceTempView("logging_table");
 
-        dataset =  dataset.select(col("level"),
-                date_format(col("datetime"),"MMMM").alias("month"),
-                date_format(col("datetime"),"M").alias("monthnum").cast(DataTypes.IntegerType));
+        Dataset<Row> result = sparkSession.sql("select level , date_format(datetime, 'MMMM') as month , " +
+                " count(1) as total" +
+                " from logging_table group by level, month order by monthNum(month) , level ");
+
+
+        result.createOrReplaceTempView("logging_table");
+
+        //dataset =  dataset.select(col("level"),
+          //      date_format(col("datetime"),"MMMM").alias("month"),
+            //    date_format(col("datetime"),"M").alias("monthnum").cast(DataTypes.IntegerType));
       //  dataset =  dataset.groupBy("level").pivot("month").count();
        // dataset =  dataset.groupBy("level").pivot("monthnum").count();
 
 
-        dataset =  dataset.groupBy("level", "month", "monthnum").count().as("total").orderBy("monthnum");
-        dataset = dataset.drop("monthnum");
+        //dataset =  dataset.groupBy("level", "month", "monthnum").count().as("total").orderBy("monthnum");
+        //dataset = dataset.drop("monthnum");
 
-        dataset.show(100);
+        result.show(100);
 
 
         Scanner scanner = new Scanner(System.in);
