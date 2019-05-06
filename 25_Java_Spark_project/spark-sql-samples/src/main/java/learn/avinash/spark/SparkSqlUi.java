39f1aa385b10fe6d5de6eab79d5882ea6d835f62
AvinashTiwari85 <qwe123kids@gmail.com>
2019-05-05 18:29:50 -0600

    no message

--- 25_Java_Spark_project/spark-sql-samples/src/main/java/learn/avinash/spark/SparkSqlUi.java
+++ 25_Java_Spark_project/spark-sql-samples/src/main/java/learn/avinash/spark/SparkSqlUi.java
@@ -22,14 +22,14 @@ public class SparkSqlUi {
                 .master("local[*]")
                 .config("spark.sql.warehouse.dir","file:///d://tmp/")
                 .getOrCreate();
-
+        sparkSession.conf().set("spark.sql.shuffle.partitions","12");
 
         Dataset<Row> dataset = sparkSession.read().option("header",true).csv("src/main/resources/biglog.txt");
         dataset.createOrReplaceTempView("logging_table");
 
         Dataset<Row> result = sparkSession.sql("select level , date_format(datetime, 'MMMM') as month , " +
-                " count(1) as total" +
-                " from logging_table group by level, month order by monthNum(month) , level ");
+           " cast(first(date_format(datetime, 'M')) as int) as monthnum , count(1) as total" +
+          " from logging_table group by level, month order by monthnum ");
 
 
         result.createOrReplaceTempView("logging_table");
