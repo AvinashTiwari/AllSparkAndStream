515487583884c08cf88a925b93c74be96d398188
AvinashTiwari85 <qwe123kids@gmail.com>
2019-05-20 19:12:52 -0600

    no message

--- 26_Java_Spark_Project/src/main/java/learn/avinash/spark/HousePricingAnlaysis.java
+++ 26_Java_Spark_Project/src/main/java/learn/avinash/spark/HousePricingAnlaysis.java
@@ -3,8 +3,10 @@ package learn.avinash.spark;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.ml.feature.VectorAssembler;
+import org.apache.spark.ml.param.ParamMap;
 import org.apache.spark.ml.regression.LinearRegression;
 import org.apache.spark.ml.regression.LinearRegressionModel;
+import org.apache.spark.ml.tuning.ParamGridBuilder;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
@@ -41,9 +43,21 @@ public class HousePricingAnlaysis {
         Dataset<Row>[] taraingTestData = modelInput.randomSplit(new double[]{0.8, 0.2});
         Dataset<Row> trainingData = taraingTestData[0];
         Dataset<Row> testData  =  taraingTestData[1];
-
+/*
         LinearRegressionModel linearRegressionModel = new LinearRegression()
+                .setMaxIter(10)
+                .setRegParam(0.3)
+                .setElasticNetParam(0.8)
                 .fit(trainingData);
+        */
+
+        LinearRegression linearRegression = new LinearRegression();
+        ParamGridBuilder paramGridBuilder = new ParamGridBuilder();
+        ParamMap[] paramMaps = paramGridBuilder.addGrid(linearRegression.regParam(),new double[]{0.01, 0.5,0.1})
+                .addGrid(linearRegression.elasticNetParam(), new double[]{0.01, 0.5,0.1})
+                .build();
+
+
         System.out.println("The training r2 value is " + linearRegressionModel.summary().r2() + " and rsme value" +  linearRegressionModel.summary().rootMeanSquaredError());
         linearRegressionModel.transform(testData)
         .show();
