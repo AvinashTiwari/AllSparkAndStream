43d73a866f16bd2211f674d7d3ff339c580a9f85
AvinashTiwari85 <qwe123kids@gmail.com>
2019-06-02 15:44:26 -0600

    no message

--- 27_Java_Spark_Project/spark-ml-casestudy/src/main/java/learn/saprk/ml/VppChapterFile.java
+++ 27_Java_Spark_Project/spark-ml-casestudy/src/main/java/learn/saprk/ml/VppChapterFile.java
@@ -2,8 +2,16 @@ package learn.saprk.ml;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.apache.spark.ml.evaluation.RegressionEvaluator;
 import org.apache.spark.ml.feature.OneHotEncoderEstimator;
 import org.apache.spark.ml.feature.StringIndexer;
+import org.apache.spark.ml.feature.VectorAssembler;
+import org.apache.spark.ml.param.ParamMap;
+import org.apache.spark.ml.regression.LinearRegression;
+import org.apache.spark.ml.regression.LinearRegressionModel;
+import org.apache.spark.ml.tuning.ParamGridBuilder;
+import org.apache.spark.ml.tuning.TrainValidationSplit;
+import org.apache.spark.ml.tuning.TrainValidationSplitModel;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
@@ -58,7 +66,38 @@ public class VppChapterFile {
                 .setOutputCols(new String[]{"payVector","countryVector","periodVector"})
         .fit(csvData)
         .transform(csvData);
-        csvData.show();
+
+        VectorAssembler vectorAssembler = new VectorAssembler();
+        Dataset<Row> inputData = vectorAssembler.setInputCols(new String[]{"firstSub", "age", "all_time_views", "last_month_views",
+                "payVector", "countryVector", "periodVector"})
+                .setOutputCol("features")
+                .transform(csvData).select("label", "features");
+
+        Dataset<Row>[] trainAndHoldOut = inputData.randomSplit(new double[]{0.9, 0.1});
+        Dataset<Row> trainAndTestData = trainAndHoldOut[0];
+        Dataset<Row> holdOutData = trainAndHoldOut[1];
+
+        LinearRegression lr = new LinearRegression();
+        ParamGridBuilder pr = new ParamGridBuilder();
+        ParamMap[] paramMap = pr.addGrid(lr.regParam(), new double[]{0.01, 0.1, 0.3, 0.5, 0.7, 1})
+                .addGrid(lr.elasticNetParam(), new double[]{0, 0.5, 1})
+                .build();
+        TrainValidationSplit tvs = new TrainValidationSplit();
+        tvs.setEstimator(lr)
+                .setEvaluator(new RegressionEvaluator().setMetricName("r2"))
+                .setEstimatorParamMaps(paramMap)
+                .setTrainRatio(0.9);
+        TrainValidationSplitModel model = tvs.fit(trainAndTestData);
+
+        LinearRegressionModel lrmodel = (LinearRegressionModel)model.bestModel();
+
+        System.out.println("The R2 Value is " + lrmodel.summary().r2());
+
+        System.out.println("coefficients : " + lrmodel.coefficients() + " intercept : " + lrmodel.intercept());
+        System.out.println("reg param : " + lrmodel.getRegParam() + " elastic net param : " + lrmodel.getElasticNetParam());
+
+
+
 
     }
 }
