f1d91751427b0a0f6f7266d08f35fd67ace5b0c5
AvinashTiwari85 <qwe123kids@gmail.com>
2019-06-05 19:01:51 -0600

    no message

---
+++ 29_Java_Spark_Project/spark-decision-tree/src/main/java/learn/spark/decision/VVPDecisionTree.java
@@ -0,0 +1,65 @@
+package learn.spark.decision;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.ml.Pipeline;
+import org.apache.spark.ml.PipelineModel;
+import org.apache.spark.ml.PipelineStage;
+import org.apache.spark.ml.evaluation.RegressionEvaluator;
+import org.apache.spark.ml.feature.OneHotEncoderEstimator;
+import org.apache.spark.ml.feature.StringIndexer;
+import org.apache.spark.ml.feature.VectorAssembler;
+import org.apache.spark.ml.param.ParamMap;
+import org.apache.spark.ml.regression.LinearRegression;
+import org.apache.spark.ml.regression.LinearRegressionModel;
+import org.apache.spark.ml.tuning.ParamGridBuilder;
+import org.apache.spark.ml.tuning.TrainValidationSplit;
+import org.apache.spark.ml.tuning.TrainValidationSplitModel;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.api.java.UDF1;
+import org.apache.spark.sql.types.DataTypes;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.apache.spark.sql.functions.callUDF;
+import static org.apache.spark.sql.functions.col;
+public class VVPDecisionTree {
+
+    public static UDF1<String,String> countryGrouping = new UDF1<String,String>() {
+
+        @Override
+        public String call(String country) throws Exception {
+            List<String> topCountries =  Arrays.asList(new String[] {"GB","US","IN","UNKNOWN"});
+            List<String> europeanCountries =  Arrays.asList(new String[] {"BE","BG","CZ","DK","DE","EE","IE","EL","ES","FR","HR","IT","CY","LV","LT","LU","HU","MT","NL","AT","PL","PT","RO","SI","SK","FI","SE","CH","IS","NO","LI","EU"});
+
+            if (topCountries.contains(country)) return country;
+            if (europeanCountries .contains(country)) return "EUROPE";
+            else return "OTHER";
+        }
+
+    };
+
+    public static void main(String[] args) {
+        System.setProperty("hadoop.home.dir", "c:/winutils");
+        Logger.getLogger("org.apache").setLevel(Level.WARN);
+
+        SparkSession spark = SparkSession.builder()
+                .appName("GymComp")
+                .config("spark.sql.warehouse.dir", "file:///D:/temp/")
+                .master("local[*]")
+                .getOrCreate();
+
+        spark.udf().register("countryGrouping", countryGrouping, DataTypes.StringType);
+
+        Dataset<Row> csvData = spark.read()
+                .option("header", true)
+                .option("inferSchema", true)
+                .csv("src/main/resources/vppFreeTrials.csv");
+
+        csvData = csvData.withColumn("country",callUDF("countryGrouping", col("country")));
+
+        csvData.show();
+    }
+    }
