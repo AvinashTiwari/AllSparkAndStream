e882365fdc1d08f3af26b2452908925f11937281
AvinashTiwari85 <qwe123kids@gmail.com>
2019-06-15 19:26:24 -0600

    no message

---
+++ 32_Java_Spark_Project/spark-d-stream/src/main/java/learn/avinash/spark/stream/LogStreamAnlaysis.java
@@ -0,0 +1,37 @@
+package learn.avinash.spark.stream;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.ml.feature.OneHotEncoderEstimator;
+import org.apache.spark.ml.feature.StringIndexer;
+import org.apache.spark.ml.feature.VectorAssembler;
+import org.apache.spark.ml.recommendation.ALS;
+import org.apache.spark.ml.recommendation.ALSModel;
+import org.apache.spark.ml.regression.LinearRegression;
+import org.apache.spark.ml.regression.LinearRegressionModel;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.streaming.Duration;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+
+public class LogStreamAnlaysis {
+    public static void main(String[] args) {
+        System.setProperty("hadoop.home.dir", "c:/winutils");
+        Logger.getLogger("org.apache").setLevel(Level.WARN);
+
+        SparkConf conf = new SparkConf().setMaster("local[*]")
+                .setAppName("sparkStream");
+        JavaStreamingContext sc = new JavaStreamingContext(conf, Durations.seconds(30));
+        JavaReceiverInputDStream<String> inputdata = sc.socketTextStream("localhost", 8989);
+/*
+        SparkSession spark = SparkSession.builder()
+                .appName("GymComp")
+                .config("spark.sql.warehouse.dir","file:///D:/temp/")
+                .master("local[*]")
+                .getOrCreate();
+*/
+    }
+}
