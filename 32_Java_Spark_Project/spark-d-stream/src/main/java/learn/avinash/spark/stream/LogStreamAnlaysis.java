38d928e78ccb6d6df0988848fd5c3a855f7f2362
AvinashTiwari85 <qwe123kids@gmail.com>
2019-06-16 14:55:08 -0600

    no message

--- 32_Java_Spark_Project/spark-d-stream/src/main/java/learn/avinash/spark/stream/LogStreamAnlaysis.java
+++ 32_Java_Spark_Project/spark-d-stream/src/main/java/learn/avinash/spark/stream/LogStreamAnlaysis.java
@@ -14,18 +14,25 @@ import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.streaming.Duration;
 import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 
 public class LogStreamAnlaysis {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.setProperty("hadoop.home.dir", "c:/winutils");
         Logger.getLogger("org.apache").setLevel(Level.WARN);
+        Logger.getLogger("org.apache.spark.storage").setLevel(Level.ERROR);
+
 
         SparkConf conf = new SparkConf().setMaster("local[*]")
                 .setAppName("sparkStream");
         JavaStreamingContext sc = new JavaStreamingContext(conf, Durations.seconds(30));
         JavaReceiverInputDStream<String> inputdata = sc.socketTextStream("localhost", 8989);
+        JavaDStream<Object> results = inputdata.map(item -> item);
+        results.print();
+        sc.start();
+        sc.awaitTermination();
 /*
         SparkSession spark = SparkSession.builder()
                 .appName("GymComp")
