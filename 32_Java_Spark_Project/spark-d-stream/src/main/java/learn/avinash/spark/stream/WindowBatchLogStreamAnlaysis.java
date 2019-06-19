a6ccc2773bc2d6573759e2bc678f04eff128a374
AvinashTiwari85 <qwe123kids@gmail.com>
2019-06-18 21:55:09 -0600

    no message

--- 32_Java_Spark_Project/spark-d-stream/src/main/java/learn/avinash/spark/stream/WindowBatchLogStreamAnlaysis.java
+++ 32_Java_Spark_Project/spark-d-stream/src/main/java/learn/avinash/spark/stream/WindowBatchLogStreamAnlaysis.java
@@ -2,16 +2,6 @@ package learn.avinash.spark.stream;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
-import org.apache.spark.ml.feature.OneHotEncoderEstimator;
-import org.apache.spark.ml.feature.StringIndexer;
-import org.apache.spark.ml.feature.VectorAssembler;
-import org.apache.spark.ml.recommendation.ALS;
-import org.apache.spark.ml.recommendation.ALSModel;
-import org.apache.spark.ml.regression.LinearRegression;
-import org.apache.spark.ml.regression.LinearRegressionModel;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;
 import org.apache.spark.streaming.Duration;
 import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.api.java.JavaDStream;
@@ -20,7 +10,7 @@ import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import scala.Tuple2;
 
-public class LogStreamAnlaysis {
+public class WindowBatchLogStreamAnlaysis {
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("hadoop.home.dir", "c:/winutils");
         Logger.getLogger("org.apache").setLevel(Level.WARN);
@@ -33,7 +23,7 @@ public class LogStreamAnlaysis {
         JavaReceiverInputDStream<String> inputdata = sc.socketTextStream("localhost", 8989);
         JavaDStream<String> results = inputdata.map(item -> item);
         JavaPairDStream<String, Long> pairDsStream = results.mapToPair(rawMessage -> new Tuple2<String, Long>(rawMessage.split(",")[0], 1L));
-        JavaPairDStream<String, Long> paitDStream = pairDsStream.reduceByKey((x, y) -> x + y);
+        JavaPairDStream<String, Long> paitDStream = pairDsStream.reduceByKeyAndWindow((x, y) -> x + y, Duration.apply(60));
         paitDStream.print();
         sc.start();
         sc.awaitTermination();
